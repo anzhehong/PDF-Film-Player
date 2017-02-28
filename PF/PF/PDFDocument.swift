5f9c88055969b59e5b4f37bfabbcc059b8720b7d
anzhehong <anzhehong@126.com>
2017-02-28 16:25:17 +0800

    support swift2.3

--- PF/PF/PDFDocument.swift
+++ PF/PF/PDFDocument.swift
@@ -59,7 +59,7 @@ public struct PDFDocument {
     private func imageFromPDFPage(pageNumber: Int) -> UIImage {
         let page = CGPDFDocumentGetPage(coreDocument, pageNumber)
         // Determine the size of the PDF page.
-        var pageRect = CGPDFPageGetBoxRect(page, CGPDFBox.MediaBox)
+        var pageRect = CGPDFPageGetBoxRect(page!, CGPDFBox.MediaBox)
         let scalingConstant: CGFloat = 240
         let pdfScale = min(scalingConstant/pageRect.size.width, scalingConstant/pageRect.size.height)
         pageRect.size = CGSizeMake(pageRect.size.width * pdfScale, pageRect.size.height * pdfScale)
@@ -71,22 +71,22 @@ public struct PDFDocument {
         let context = UIGraphicsGetCurrentContext()
         
         // First fill the background with white.
-        CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 1.0)
-        CGContextFillRect(context,pageRect)
+        CGContextSetRGBFillColor(context!, 1.0, 1.0, 1.0, 1.0)
+        CGContextFillRect(context!,pageRect)
         
-        CGContextSaveGState(context)
+        CGContextSaveGState(context!)
         // Flip the context so that the PDF page is rendered right side up.
-        CGContextTranslateCTM(context, 0.0, pageRect.size.height)
-        CGContextScaleCTM(context, 1.0, -1.0)
+        CGContextTranslateCTM(context!, 0.0, pageRect.size.height)
+        CGContextScaleCTM(context!, 1.0, -1.0)
         
         // Scale the context so that the PDF page is rendered at the correct size for the zoom level.
-        CGContextScaleCTM(context, pdfScale, pdfScale)
-        CGContextDrawPDFPage(context, page)
-        CGContextRestoreGState(context)
+        CGContextScaleCTM(context!, pdfScale, pdfScale)
+        CGContextDrawPDFPage(context!, page!)
+        CGContextRestoreGState(context!)
         
         let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        return backgroundImage
+        return backgroundImage!
     }
 }
